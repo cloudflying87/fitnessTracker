eecbaec0405a0592f5dc5438f7a62c7270b45200
unknown <davidhale87@gmail.com>
2020-08-09 18:03:50 -0500

    Updating server for Heroku deployment

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
+
 require("./routes/htmlRoutes")(app);
 require("./routes/apiRoutes")(app);
 
